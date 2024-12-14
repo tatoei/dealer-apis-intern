dcccf1ea33333d215e6e1b2e8e5bcbcaa69a864b
tatoei <159608764+tatoei@users.noreply.github.com>
2024-12-14 19:14:39 +0700

    chore: swagger

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    // .setTitle('Cats example')
-    // .setDescription('The cats API description')
-    .setVersion('1.0')
-    // .addTag('cats')
-    .build();
+  const config = new DocumentBuilder().setVersion('1.0').build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
